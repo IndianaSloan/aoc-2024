2b3e523c4818f1ae7e962faaae308a903668c3f1
Ciaran Sloan <ciaransloan87@gmail.com>
2024-12-01 07:31:04 +0000

    day1-part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,21 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val firstList = mutableListOf<Int>()
+        val secondList = mutableListOf<Int>()
+        input.forEach {
+            val numbers = it.split("   ").map(String::toInt)
+            firstList.add(numbers[0])
+            secondList.add(numbers[1])
+        }
+
+        val sortedFirst = firstList.sorted()
+        val sortedSecond = secondList.sorted()
+        val result = sortedFirst.mapIndexed { index, i -> abs(i - sortedSecond[index]) }
+            .sum()
+        return result
+        //return input.size
     }
 
     fun part2(input: List<String>): Int {
@@ -8,14 +23,10 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("input/test_input")) == 11)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("input/Day01")
     part1(input).println()
     part2(input).println()
 }
