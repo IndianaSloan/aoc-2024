31c9d36a81af2b8f0dd8954f04f4b9f99771a5cc
Ciaran Sloan <ciaransloan87@gmail.com>
2024-12-02 13:00:02 +0000

    day02-part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,27 +1,49 @@
 import kotlin.math.abs
 
 fun main() {
+
     fun part1(input: List<String>): Int {
-        val safeRange = (1..3)
         return input
             .map { it.split(" ").map(String::toInt) }
-            .count { levels ->
-                val isAscending = levels.zipWithNext { a, b -> a < b }.all { it }
-                val isDescending = levels.zipWithNext { a, b -> a > b }.all { it }
-                val isSafeRange = levels.zipWithNext { a, b -> abs(a - b) in (safeRange) }.all { it }
-                (isAscending || isDescending) && isSafeRange
-            }
+            .count { levels -> levels.isSafe() }
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return input
+            .map { it.split(" ").map(String::toInt) }
+            .count { levels -> levels.isSafeWithDampening() }
     }
 
     // Test if implementation meets criteria from the description, like:
     check(part1(readInput("input/test_input")) == 2)
+    check(part2(readInput("input/test_input")) == 4)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("input/Day02")
     part1(input).println()
     part2(input).println()
+}
+
+private val safeRange = (1..3)
+
+private fun List<Int>.isSafe(): Boolean {
+    val isAscending = zipWithNext { a, b -> a < b }.all { it }
+    val isDescending = zipWithNext { a, b -> a > b }.all { it }
+    val isSafeRange = zipWithNext { a, b -> abs(a - b) in (safeRange) }.all { it }
+    return (isAscending || isDescending) && isSafeRange
+}
+
+private fun List<Int>.isSafeWithDampening(): Boolean {
+    if (isSafe()) {
+        return true
+    }
+
+    forEachIndexed { index, _ ->
+        val list = this.toMutableList()
+        list.removeAt(index)
+        if (list.isSafe()) {
+            return true
+        }
+    }
+    return false
 }
