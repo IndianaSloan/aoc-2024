db3c2ed22df028c6bdb5d8c776dfd0f708a618a2
Ciaran Sloan <ciaransloan87@gmail.com>
2024-12-04 11:23:57 +0000

    day04-part2

--- src/Day04.kt
+++ src/Day04.kt
@@ -8,48 +8,49 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-
-        return 0
+        val points = findCrossMatches(input)
+        return points.count()
     }
 
     // Test if implementation meets criteria from the description, like:
     check(part1(readInput("input/test_input")) == 18)
+    check(part2(readInput("input/test_input")) == 9)
 
     val input = readInput("input/Day04")
     part1(input).println()
+    part2(input).println()
 }
 
-private const val WORD_MATCH = "XMAS"
-
-fun findMatches(grid: List<String>): List<Point> {
-    val matches = mutableListOf<Point>()
-    val rowRange = 0 until grid[0].length - WORD_MATCH.length + 1
-    val rowRangeReversed = grid[0].lastIndex downTo WORD_MATCH.length - 1
-    val columnRange = 0 until grid.size - WORD_MATCH.length + 1
-    val columnRangeReversed = grid.lastIndex downTo WORD_MATCH.length - 1
-
-    fun checkWordMatch(x: Int, y: Int, xOffset: Int, yOffset: Int): Boolean {
-        for (i in WORD_MATCH.indices) {
-            val nextX = x + (xOffset * i)
-            val nextY = y + (yOffset * i)
-            if (nextY !in grid.indices || nextX !in grid[0].indices || grid[nextY][nextX] != WORD_MATCH[i]) {
-                return false
-            }
+fun checkWordMatch(grid: List<String>, word: String, x: Int, y: Int, xOffset: Int, yOffset: Int): Boolean {
+    for (i in word.indices) {
+        val nextX = x + (xOffset * i)
+        val nextY = y + (yOffset * i)
+        if (nextY !in grid.indices || nextX !in grid[0].indices || grid[nextY][nextX] != word[i]) {
+            return false
         }
-        return true
     }
+    return true
+}
+
+private fun findMatches(grid: List<String>): List<Point> {
+    val word = "XMAS"
+    val matches = mutableListOf<Point>()
+    val rowRange = 0 until grid[0].length - word.length + 1
+    val rowRangeReversed = grid[0].lastIndex downTo word.length - 1
+    val columnRange = 0 until grid.size - word.length + 1
+    val columnRangeReversed = grid.lastIndex downTo word.length - 1
 
     // Horizontal
     for (y in grid.indices) {
         // Left -> Right
         for (x in rowRange) {
-            if (checkWordMatch(x, y, 1, 0)) {
+            if (checkWordMatch(grid, word, x, y, 1, 0)) {
                 matches.add(Point(x, y))
             }
         }
         // Right -> Left
         for (x in rowRangeReversed) {
-            if (checkWordMatch(x, y, -1, 0)) {
+            if (checkWordMatch(grid, word, x, y, -1, 0)) {
                 matches.add(Point(x, y))
             }
         }
@@ -59,13 +60,13 @@ fun findMatches(grid: List<String>): List<Point> {
     for (x in grid[0].indices) {
         // Top -> Bottom
         for (y in columnRange) {
-            if (checkWordMatch(x, y, 0, 1)) {
+            if (checkWordMatch(grid, word, x, y, 0, 1)) {
                 matches.add(Point(x, y))
             }
         }
         // Bottom -> Top
         for (y in columnRangeReversed) {
-            if (checkWordMatch(x, y, 0, -1)) {
+            if (checkWordMatch(grid, word, x, y, 0, -1)) {
                 matches.add(Point(x, y))
             }
         }
@@ -74,7 +75,7 @@ fun findMatches(grid: List<String>): List<Point> {
     // Top Left -> Bottom Right
     for (y in columnRange) {
         for (x in rowRange) {
-            if (checkWordMatch(x, y, 1, 1)) {
+            if (checkWordMatch(grid, word, x, y, 1, 1)) {
                 matches.add(Point(x, y))
             }
         }
@@ -82,7 +83,7 @@ fun findMatches(grid: List<String>): List<Point> {
     // Top Right -> Bottom Left
     for (y in columnRange) {
         for (x in rowRangeReversed) {
-            if (checkWordMatch(x, y, -1, 1)) {
+            if (checkWordMatch(grid, word, x, y, -1, 1)) {
                 matches.add(Point(x, y))
             }
         }
@@ -91,7 +92,7 @@ fun findMatches(grid: List<String>): List<Point> {
     // Bottom Left -> Top Right
     for (y in columnRangeReversed) {
         for (x in rowRange) {
-            if (checkWordMatch(x, y, 1, -1)) {
+            if (checkWordMatch(grid, word, x, y, 1, -1)) {
                 matches.add(Point(x, y))
             }
         }
@@ -100,10 +101,43 @@ fun findMatches(grid: List<String>): List<Point> {
     // Bottom Right -> Top Left
     for (y in columnRangeReversed) {
         for (x in rowRangeReversed) {
-            if (checkWordMatch(x, y, -1, -1)) {
+            if (checkWordMatch(grid, word, x, y, -1, -1)) {
                 matches.add(Point(x, y))
             }
         }
     }
     return matches
+}
+
+private fun findCrossMatches(grid: List<String>): List<Point> {
+    val matches = mutableListOf<Point>()
+    val rowRange = 1 until grid[0].length - 1
+    val columnRange = 1 until grid.size - 1
+
+    for (y in columnRange) {
+        for (x in rowRange) {
+            if (grid[y][x] == 'A') {
+                if (checkCrossMatch(grid, x, y)) {
+                    matches.add(Point(x, y))
+                }
+            }
+        }
+    }
+
+    return matches
+}
+
+private fun checkCrossMatch(grid: List<String>, centerX: Int, centerY: Int): Boolean {
+    val word = "MAS"
+    // Top Left -> Bottom Right
+    val ltr = checkWordMatch(grid, word, centerX - 1, centerY - 1, 1, 1) ||
+            // Bottom Right -> Top Left
+            checkWordMatch(grid, word, centerX + 1, centerY + 1, -1, -1)
+
+    // Top Right -> Bottom Left
+    val rtl = checkWordMatch(grid, word, centerX + 1, centerY - 1, -1, 1) ||
+    // Bottom Left -> Top Right
+        checkWordMatch(grid, word, centerX -1, centerY + 1, 1, -1)
+
+    return ltr && rtl
 }
