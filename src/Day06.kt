ecae2410ee4dcc6a3d2b1df56e2b3f383696e2d9
Ciaran Sloan <ciaransloan87@gmail.com>
2024-12-06 17:18:39 +0000

    day06-part1-improve

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,73 +1,42 @@
 import Direction.*
 import java.awt.Point
-import kotlin.math.abs
-
-enum class Direction { UP, DOWN, LEFT, RIGHT, }
 
 fun main() {
 
-    fun findNextBlocker(
-        gridSize: Point,
-        points: List<Point>,
-        position: Pair<Point, Direction?>
-    ): Pair<Point, Direction?> {
-        val currentPosition = position.first
-        return when (position.second) {
-            UP -> {
-                val point = points.filter { it.y < currentPosition.y && it.x == currentPosition.x }.maxByOrNull { it.y }
-                if (point != null) {
-                    Point(point.x, point.y + 1) to RIGHT
-                } else Point(currentPosition.x, 0) to null
-            }
-            DOWN -> {
-                val point = points.filter { it.y > currentPosition.y && it.x == currentPosition.x }.minByOrNull { it.y }
-                if (point != null) {
-                    Point(point.x, point.y - 1) to LEFT
-                } else Point(currentPosition.x, gridSize.y) to null
-            }
-            LEFT -> {
-                val point = points.filter { it.x < currentPosition.x && it.y == currentPosition.y }.maxByOrNull { it.x }
-                if (point != null) {
-                    Point(point.x + 1, point.y) to UP
-                } else Point(0, currentPosition.y) to null
-            }
-            RIGHT -> {
-                val point = points.filter { it.x > currentPosition.x && it.y == currentPosition.y }.minByOrNull { it.x }
-                if (point != null) {
-                    Point(point.x - 1, point.y) to DOWN
-                } else Point(gridSize.x, currentPosition.y) to null
-            }
-            else -> position
-        }
-    }
+    var currentPos = Position(0, 0, UP)
+    val obstacles = mutableListOf<Point>()
+    val pointsVisited = mutableListOf<Position>()
 
-    fun part1(input: List<String>): Int {
-        val gridSize = Point(input.first().lastIndex, input.lastIndex)
-        val blockers = mutableListOf<Point>()
-        var currentPosition: Pair<Point, Direction?> = Pair(Point(0, 0), UP)
-        val pointsVisited = mutableListOf<Point>()
+    fun parseInput(input: List<String>) {
+        obstacles.clear()
+        pointsVisited.clear()
         input.forEachIndexed { y, line ->
             line.forEachIndexed { x, char ->
                 if (char == '#') {
-                    blockers.add(Point(x, y))
+                    obstacles.add(Point(x, y))
                 }
             }
             if (line.contains('^')) {
                 val x = line.indexOfFirst { it == '^' }
-                currentPosition = Pair(Point(x, y), UP)
+                currentPos = Position(x, y, UP)
             }
         }
-        while (currentPosition.second != null) {
-            val nextPosition = findNextBlocker(gridSize, blockers.toList(), currentPosition)
-            val pointsTouched = currentPosition.first.pointsTo(nextPosition.first)
-            pointsVisited.addAll(pointsTouched)
-            currentPosition = nextPosition
+    }
+
+    fun part1(input: List<String>): Int {
+        parseInput(input)
+        pointsVisited.add(currentPos)
+        while (!currentPos.canEscape(input)) {
+            currentPos.move()
+            pointsVisited.add(currentPos.copy())
+            if (!currentPos.canMove(input)) {
+                currentPos.turn()
+            }
         }
-        return pointsVisited.toSet().count()
+        return pointsVisited.map { Point(it.x, it.y) }.toSet().count()
     }
 
     fun part2(input: List<String>): Int {
-
         return 0
     }
 
@@ -76,22 +45,49 @@ fun main() {
 
     val input = readInput("input/Day06")
     part1(input).println()
-    part2(input).println()
+    //part2(input).println()
 }
 
-fun Point.pointsTo(other: Point): List<Point> {
-    val points = when {
-        x != other.x -> {
-            val range = if (x < other.x) x..other.x else x downTo other.x
-            range.map { newX -> Point(newX, y) }
-        }
+private enum class Direction { UP, DOWN, LEFT, RIGHT }
 
-        y != other.y -> {
-            val range = if (y < other.y) y..other.y else y downTo other.y
-            range.map { newY -> Point(x, newY) }
-        }
+private data class Position(var x: Int, var y: Int, var direction: Direction?) {
 
-        else -> emptyList()
+    fun move() {
+        when (direction) {
+            UP -> y -= 1
+            DOWN -> y += 1
+            LEFT -> x -= 1
+            RIGHT -> x += 1
+            else -> {} // Do nothing
+        }
     }
-    return points
+
+    fun canMove(grid: List<String>): Boolean {
+        return if (x == 0 || x == grid.first().lastIndex || y == 0 || y == grid.lastIndex) {
+            false
+        } else {
+            val nextPoint = when (direction) {
+                UP -> grid[y - 1][x]
+                DOWN -> grid[y + 1][x]
+                LEFT -> grid[y][x - 1]
+                RIGHT -> grid[y][x + 1]
+                null -> return false
+            }
+            nextPoint != '#'
+        }
+    }
+
+    fun canEscape(grid: List<String>): Boolean {
+        return x == 0 || x == grid.first().lastIndex || y == 0 || y == grid.lastIndex
+    }
+
+    fun turn() {
+        direction = when (direction) {
+            UP -> RIGHT
+            DOWN -> LEFT
+            LEFT -> UP
+            RIGHT -> DOWN
+            else -> null
+        }
+    }
 }
