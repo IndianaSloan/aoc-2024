5b188b96d11b0b985bd0f6038c80f6874fbdba33
Ciaran Sloan <ciaransloan87@gmail.com>
2024-12-09 21:02:04 +0000

    day09-part2-failed

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,4 +1,3 @@
-import java.awt.Point
 import kotlin.ranges.IntRange.Companion.EMPTY
 
 fun main() {
@@ -15,20 +14,64 @@ fun main() {
                 count++
             }
         }
-        println(diskMap.joinToString(""))
         return diskMap
     }
 
     fun deFragment(fileMap: List<String>): List<String> {
         val mutableDiskMap = fileMap.toMutableList()
-        while(mutableDiskMap.indexOfFirst { it == FREE_SPACE } < mutableDiskMap.indexOfLast { it.toLongOrNull() != null }) {
-            val nextDigitIndex = mutableDiskMap.indexOfLast { it.toLongOrNull() != null }
+        while (mutableDiskMap.indexOfFirst { it == FREE_SPACE } < mutableDiskMap.indexOfLast { it.toLongOrNull() != null }) {
+            val nextDigitIndex = mutableDiskMap.indexOfLast { it != FREE_SPACE }
             val value = mutableDiskMap[nextDigitIndex]
             val nextSpaceIndex = mutableDiskMap.indexOfFirst { it == FREE_SPACE }
             mutableDiskMap.removeAt(nextSpaceIndex)
             mutableDiskMap.add(nextSpaceIndex, value)
             mutableDiskMap.removeAt(nextDigitIndex)
             mutableDiskMap.add(nextDigitIndex, FREE_SPACE)
+        }
+        return mutableDiskMap
+    }
+
+    fun deFragmentEqualSpace(fileMap: List<String>): List<String> {
+        val mutableDiskMap = fileMap.toMutableList()
+        val needsPlaced = mutableListOf<List<String>>()
+        while (mutableDiskMap.indexOfFirst { it == FREE_SPACE } < mutableDiskMap.indexOfLast { it.toLongOrNull() != null }) {
+            val nextDigitEndIndex = mutableDiskMap.indexOfLast { it != FREE_SPACE }
+            val nextDigitValue = mutableDiskMap[nextDigitEndIndex]
+            val nextDigitStartIndex = mutableDiskMap.indexOfFirst { it == nextDigitValue }
+            val requiredFreeSpace = nextDigitEndIndex - nextDigitStartIndex + 1
+
+            var freeSpaceStartIndex = 0
+            var freeSpaceEndIndex = 0
+
+            var currentStartValue = mutableDiskMap[freeSpaceStartIndex]
+            while (currentStartValue != FREE_SPACE) {
+                freeSpaceStartIndex++
+                currentStartValue = mutableDiskMap[freeSpaceStartIndex]
+            }
+            freeSpaceEndIndex = freeSpaceStartIndex
+            var currentEndValue = mutableDiskMap[freeSpaceEndIndex]
+            while (currentEndValue == FREE_SPACE) {
+                freeSpaceEndIndex++
+                currentEndValue = mutableDiskMap[freeSpaceEndIndex]
+                if (currentEndValue != FREE_SPACE) {
+                    freeSpaceEndIndex--
+                }
+            }
+            if (freeSpaceEndIndex - freeSpaceStartIndex >= requiredFreeSpace) {
+                (0 until requiredFreeSpace).forEach { index ->
+                    mutableDiskMap.removeAt(freeSpaceStartIndex + index)
+                    mutableDiskMap.add(freeSpaceStartIndex + index, nextDigitValue)
+                    mutableDiskMap.removeAt(nextDigitStartIndex + index)
+                    mutableDiskMap.add(nextDigitStartIndex + index, FREE_SPACE)
+                }
+            } else {
+                val removedItems = mutableListOf<String>()
+                ((requiredFreeSpace - 1) downTo 0).forEach { index ->
+                    mutableDiskMap.removeAt(nextDigitStartIndex + index)
+                    removedItems.add(nextDigitValue)
+                }
+                needsPlaced.add(removedItems)
+            }
         }
         return mutableDiskMap
     }
@@ -41,14 +84,15 @@ fun main() {
             .sum()
     }
 
-    fun part2(input: List<String>): Int {
-
+    fun part2(input: List<String>): Long {
+        val diskMap = parseInput(input)
+        val deFragmentedDiskMap = deFragmentEqualSpace(diskMap)
         return 0
     }
 
     // Test if implementation meets criteria from the description, like:
-    //check(part1(readInput("input/test_input")) == 1928)
-    //check(part2(readInput("input/test_input")) == 34)
+    check(part1(readInput("input/test_input")) == 1928L)
+    check(part2(readInput("input/test_input")) == 2858L)
 
     val input = readInput("input/Day09")
     part1(input).println()
